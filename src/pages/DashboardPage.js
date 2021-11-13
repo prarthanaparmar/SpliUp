7784fb938027a6b07af644ceb5a3b8106ced119e
heenalsapovadia <heenalsapovadia@gmail.com>
2021-11-12 23:53:02 -0400

    APi check

--- src/pages/DashboardPage.js
+++ src/pages/DashboardPage.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useCallback, useEffect, useState } from "react";
 
 import DashboardTop from "../components/DashboardTop";
 import Expenses from "../components/Expenses";
@@ -9,10 +9,25 @@ const DashboardPage = () => {
   const [youOwe, setYouOwe] = useState(0.0);
   const [youAreOwed, setYouAreOwed] = useState(0.0);
 
-  const totalBalanceHandler = () => {
+  const totalBalanceHandler = useCallback(async () => {
     // fetch total balance from dynamo
+    try {
+      const response = await fetch("http://localhost:3001/friends/totalSum");
+      if (!response.ok) {
+        throw new Error("SOmething went wrong");
+      }
+      const data = await response.json();
+      console.log("message received - ", data.message);
+      setTotalBalance(data.message);
+    } catch (error) {
+      console.log("Error : ", error.message);
+    }
     // setTotalBalance()
-  };
+  });
+
+  useEffect(() => {
+    totalBalanceHandler();
+  }, []);
 
   const youOweHandler = () => {};
 
