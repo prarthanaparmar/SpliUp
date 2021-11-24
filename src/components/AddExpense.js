8a0c624c1943371a0a8c041f891b2bd61688fb4c
heenalsapovadia <heenalsapovadia@gmail.com>
2021-11-24 17:56:32 -0400

    Added placeholders for future comps

--- src/components/AddExpense.js
+++ src/components/AddExpense.js
@@ -6,8 +6,13 @@ const AddExpense = (props) => {
       <h2>Add Expenses here...</h2>
       <form>
         <input type="text" placeholder="Enter name or email address" />
+        {/* dynamic dropdown for friends list */}
         <input type="text" placeholder="Enter a Description" />
         <input type="number" placeholder="0.0" />
+        {/* currency drop down */}
+        {/* dynamic paid by dropdown */}
+        {/* fields for splitting proportion */}
+        {/* date field */}
         <button>Add</button>
       </form>
       <button onClick={props.onClose}>Close</button>
